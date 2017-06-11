b9479c8928c68fa71a1f0f0b4979d6a38336572c
Dejan <dejan@Bixues-MacBook-Air-2.local>
2017-06-11 00:02:59 -0400

    changed main to functions

--- main.py
+++ main.py
@@ -2,33 +2,48 @@ import puz
 from clue import Clue
 from cell import Cell
 
-p = puz.read('example.puz')
+def save_puzzle(date):
+  filename = date + ".puz"
+  url = "http://www.jacobshufro.com/xwords2/puzs/" + filename
+  response = requests.get(url)
 
-numbering = p.clue_numbering()
-cluesAcross = []
-cluesDown = []
+  with open("puzs/" + filename, 'w') as f:
+    the_file.write(response.text)
+    
+def create_clues_across():
+	p = puz.read('example.puz')
+	numbering = p.clue_numbering()
+	cluesAcross = []
+	for clue in numbering.across:
+		answer = ''.join(
+	        p.solution[clue['cell'] + i]
+	        for i in range(clue['len']))
+		cluesAcross.append(Clue(clue['num'], True, clue['clue'], answer, False))
+	return cluesAcross
 
-for clue in numbering.across:
-	answer = ''.join(
-        p.solution[clue['cell'] + i]
-        for i in range(clue['len']))
-	cluesAcross.append(Clue(clue['num'], True, clue['clue'], answer, False))
+def create_clues_down():
+	p = puz.read('example.puz')
+	numbering = p.clue_numbering()
+	cluesDown = []
+	for clue in numbering.down:
+		answer = ''.join(
+	        p.solution[clue['cell'] + i]
+	        for i in range(clue['len']))
+		cluesDown.append(Clue(clue['num'], False, clue['clue'], answer, False))
+	return cluesDown
 
-for clue in numbering.down:
-	answer = ''.join(
-        p.solution[clue['cell'] + i]
-        for i in range(clue['len']))
-	cluesDown.append(Clue(clue['num'], False, clue['clue'], answer, False))
 
 
-state = [[None for i in range(p.height)] for i in range(p.height)]
-
-for row in range(p.height):
-	for col in range(p.height):
-		value = p.fill[row*p.width + col]
-		if value == "." :
-			state[row][col] = Cell("BLACK", False, None, None)
-		elif (row == 0 or col == 0 or state[row-1][col-1].variety == "BLACK") :
-			state[row][col] = Cell("WHITE", True, None, p.solution[row*p.width + col])
-		else :
-			state[row][col] = Cell("WHITE", False, None, p.solution[row*p.width + col])
+def create_state():
+	p = puz.read('example.puz')
+	state = [[None for i in range(p.height)] for i in range(p.height)]
+	for row in range(p.height):
+		for col in range(p.height):
+			value = p.fill[row*p.width + col]
+			if value == "." :
+				state[row][col] = Cell("BLACK", False, None, None)
+			elif (row == 0 or col == 0 or state[row-1][col-1].variety == "BLACK") :
+				state[row][col] = Cell("WHITE", True, None, p.solution[row*p.width + col])
+			else :
+				state[row][col] = Cell("WHITE", False, None, p.solution[row*p.width + col])
+	return state
