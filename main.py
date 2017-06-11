ea5ca72618cb4f073c3658635803399eb72587ef
Dejan <dejan@Bixues-MacBook-Air-2.local>
2017-06-10 23:20:25 -0400

    create puzzle structures from file

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import puz
 
+class Clue:
+	def __init__(self, number, across, clue, answer):
+ 		self.number = number
+		self.across = across
+		self.clue = clue
+		self.answer = answer
+
+
 p = puz.read('example.puz')
 
 numbering = p.clue_numbering()
+cluesAcross = []
+cluesDown = []
 
-print 'Across'
 for clue in numbering.across:
-    answer = ''.join(
+	answer = ''.join(
         p.solution[clue['cell'] + i]
         for i in range(clue['len']))
-    print clue['num'], clue['clue'], '-', answer
+	cluesAcross.append(Clue(clue['num'], True, clue['clue'], answer))
 
-print 'Down'
 for clue in numbering.down:
-    answer = ''.join(
-        p.solution[clue['cell'] + i * numbering.width]
+	answer = ''.join(
+        p.solution[clue['cell'] + i]
         for i in range(clue['len']))
-    print clue['num'], clue['clue'], '-', answer
+	cluesDown.append(Clue(clue['num'], False, clue['clue'], answer))
+
+
+state = [[None for i in range(p.height)] for i in range(p.height)]
 
 for row in range(p.height):
-    cell = row * p.width
-    # Substitute p.solution for p.fill to print the answers
-    print ' '.join(p.fill[cell:cell + p.width])
+	for col in range(p.height):
+		state[row][col] = p.fill[row*p.width + col]
