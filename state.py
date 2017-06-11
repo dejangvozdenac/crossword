2c35324c14c01209ae4d592246915c2cd38c515b
Dejan <dejan@Bixues-MacBook-Air-2.local>
2017-06-11 00:43:06 -0400

    added submission on backend

--- state.py
+++ state.py
@@ -4,12 +4,54 @@ import puz
 class State:
   def __init__(self, puzzle):
     self.array = [[None for i in range(puzzle.height)] for i in range(puzzle.height)]
+    number = 1
     for row in range(puzzle.height):
 		for col in range(puzzle.height):
 			value = puzzle.fill[row*puzzle.width + col]
 			if value == "." :
-				self.array[row][col] = Cell("BLACK", False, None, None)
+				self.array[row][col] = Cell("BLACK", None, None, None)
 			elif (row == 0 or col == 0 or self.array[row][col - 1].variety == "BLACK" or self.array[row - 1][col].variety == "BLACK"):
-				self.array[row][col] = Cell("WHITE", True, None, puzzle.solution[row*puzzle.width + col])
+				self.array[row][col] = Cell("WHITE", number, None, puzzle.solution[row*puzzle.width + col])
+				number += 1
 			else :
-				self.array[row][col] = Cell("WHITE", False, None, puzzle.solution[row*puzzle.width + col])
+				self.array[row][col] = Cell("WHITE", None, None, puzzle.solution[row*puzzle.width + col])
+
+	def parse_number(number):
+		for row in range(len(self.array)):
+			for col in range(len(self.array)):
+				if self.array[row][col].numbered == number
+					return row, col
+
+	def submit_letter_exact(row, col, letter):
+		if self.array[row][col].variety == "WHITE":
+			self.array[row][col].letter = letter
+
+	def submit_letter(place, offset, letter):
+		spl = place.split()
+		number = int(spl[0])
+		across = (spl[1] == "a")
+		row, col = parse_number(number)
+		if across:
+			submit_letter_exact(row, col + offset, letter)
+		else:
+			submit_letter_exact(row + offset, col, letter)
+
+	def submit_word(place, word):
+		spl = place.split()
+		number = int(spl[0])
+		across = (spl[1] == "a")
+		row, col = parse_number(number)
+		for i in range(len(word)):
+			submit_letter_exact(row, col, word[i])
+			if across:
+				col += 1
+			else:
+				row += 1
+
+	def check_solution():
+		for row in range(len(self.array)):
+			for col in range(len(self.array)):
+				if self.array[row][col].letter != self.array[row][col].solution
+					return False
+		return True
+
