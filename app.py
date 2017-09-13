19fee8e0d5d210899b4d1700716e6c189cfa6d22
Jiaao Hou <jiaao.hou@yale.edu>
2017-09-13 00:08:24 -0700

    multiple rooms infra, but rooms not stored separately yet

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import datetime
 
 app = Flask(__name__)
 
+rooms = {}
 cluesAcross = None
 cluesDown = None
 state = None
@@ -18,10 +19,11 @@ check_displayed = True
 def new_puzzle():
   if request.method == "GET":
     today = datetime.date.today().strftime("%y.%m.%d")
-    return render_template('new_puzzle.html', today=today)
+    return render_template('new_puzzle.html', today=today, room=request.args["room"])
 
   elif request.method == "POST":
     date = request.form['date']
+    room = request.form['room']
 
     global cluesAcross, cluesDown, state, check, check_displayed
     cluesAcross = parser.create_clues_across(date)
@@ -31,23 +33,45 @@ def new_puzzle():
     check = None
     check_displayed = True
 
-    return redirect(url_for("index"))
+    return redirect(room)
 
 @app.route("/")
 def index():
-  global state, cluesAcross, cluesDown, check, check_displayed
+  if request.args.get("room"):
+    return redirect(request.args["room"])
 
-  # puzzle is not set up
+  return redirect(url_for("join"))
+
+@app.route("/join", methods=["GET", "POST"])
+def join():
+  if request.method == "GET":
+    return render_template("join_room.html")
+  elif request.method == "POST":
+    room = request.form['room']
+    rooms[room] = "placeholder" # TODO initialization
+    return redirect(room)
+
+@app.route("/<room_name>/")
+def room(room_name):
+  global state, cluesAcross, cluesDown, check, check_displayed, rooms
+
+  if room_name not in rooms:
+    return redirect(url_for("join"))
+
+  # if puzzle is not set up
   if not cluesAcross:
-    return redirect(url_for("new_puzzle"))
+    return redirect(url_for("new_puzzle", room=room_name))
 
-  return render_template('index.html', state=state, cluesAcross=cluesAcross, cluesDown=cluesDown, check=check)
+  return render_template("index.html", state=state, cluesAcross=cluesAcross, cluesDown=cluesDown, check=check, room=room_name)
 
 @app.route("/command/", methods=["POST"])
 def command():
+  room = request.args.get("room")
+  if not room:
+    raise Exception("Jay fucked up!") # TODO(jay): remove this after testing
+
   global state, check, check_displayed
   
-
   clue = request.form['clue']
   position = request.form['position']
   solution = request.form['solution'].upper()
@@ -75,11 +99,13 @@ def command():
     state.uncheck_solution()
   elif command_type == "New Puzzle":
     return redirect(url_for("new_puzzle"))
+  elif command_type == "Switch Room":
+    return redirect(url_for("join"))
 
   if check != None:
     state.check_solution()
 
-  return redirect(url_for("index"))
+  return redirect(url_for("index", room=room))
 
 if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
