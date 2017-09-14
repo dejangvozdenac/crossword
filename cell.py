1eec045d1f3b82e0a89f1f6bc1e07cb57f12dfc7
Jiaao Hou <jiaao.hou@yale.edu>
2017-09-13 23:04:33 -0700

    rename Cell.circle to Cell.circled

--- cell.py
+++ cell.py
@@ -1,7 +1,7 @@
 class Cell:
-  def __init__(self, variety, numbered, content, answer, circle):
+  def __init__(self, variety, numbered, content, answer, circled):
     self.variety = variety
     self.numbered = numbered
     self.content = content
     self.answer = answer
-    self.circle = circle
+    self.circled = circled
