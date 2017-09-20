db5868b4a563941f88917802a0f0b324b267b4fa
Jiaao Hou <jiaao.hou@yale.edu>
2017-09-20 07:34:09 -0700

    fixed #3 uncheck

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def command():
       state.submit(Submission.DELETE_WORD, clue)
   elif command_type == "Check":
     state.check_solution()
-  elif command_tyse == "Uncheck":
+  elif command_type == "Uncheck":
     state.uncheck_solution()
   elif command_type == "New Puzzle":
     return redirect(url_for("new_puzzle", room_name=room_name))
